a6c2e34a48e9725188d0156a654440b749b3a85b
Bill Kelly <billfk1989@gmail.com>
2018-06-01 14:13:20 -0500

    Add routes for new book, edit book, and delete book.

--- BookReviewMain.py
+++ BookReviewMain.py
@@ -11,6 +11,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
+# Main page for Website, Shows all the book
 @app.route('/')
 @app.route('/books/')
 def bookList():
@@ -20,6 +21,49 @@ def bookList():
 	for book in books:
 		output += book.title
 		output += '</br>'
+	return output
+
+# Route for individual book information
+@app.route('/books/<int:book_id>/')
+def bookInformaion(book_id):
+	books = session.query(Book).filter_by(id = book_id)
+	output = ''
+	for book in books:
+		output += '<h1>'
+		output += book.title
+		output += '</h1>'
+		output += 'Author: ' + str(book.author)
+		if book.subject is not None:
+			output += 'Subject: ' + str(book.subject)
+		if book.category is not None:
+			output += 'Category: ' + str(book.category)
+		if book.summary is not None:
+			output += 'Summary; ' + str(book.summary)
+		output += '</br>'
+	return output
+
+
+# Route for adding a new book
+@app.route('/books/new', methods = ['GET', 'POST'])
+def newBook():
+	output = ''
+	output += 'Page for adding a new book'
+	return output
+
+# Route for editing book information
+@app.route('/books/<int:book_id>/edit', methods = ['GET', 'POST'])
+def editBook(book_id):
+	editBook = session.query(Book).filter_by(id = book_id).one()
+	output = ''
+	output += 'Page for editing information of ' + str(editBook.title)
+	return output
+
+# Route for deleting book information
+@app.route('/books/<int:book_id>/delete', methods = ['GET', 'POST'])
+def deleteBook(book_id):
+	deleteBook = session.query(Book).filter_by(id = book_id).one()
+	output = ''
+	output += 'Page for deleting ' + str(deleteBook.title)
 	return output
 
 	

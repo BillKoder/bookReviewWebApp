644e9b3a88b1f9bb8b07b939be0b320771c38058
vagrant <vagrant@vagrant.vm>
2018-06-15 18:50:35 +0000

    Add Function to edit Books

--- BookReviewMain.py
+++ BookReviewMain.py
@@ -17,34 +17,12 @@ session = DBSession()
 def bookList():
 	books = session.query(Book)
 	return render_template('bookList.html', books = books)
-	#output = ''
-	#output += '<h1>Books</h1>'
-	#for book in books:
-	#	output += book.title
-	#	output += '</br>'
-	#return output
 
 # Route for individual book information
 @app.route('/books/<int:book_id>/')
 def bookInformation(book_id):
 	book = session.query(Book).filter_by(id = book_id).one()
 	return render_template('bookinfo.html', book = book)
-
-	#output = ''
-	#for book in books:
-	#	output += '<h1>'
-	#	output += book.title
-	#	output += '</h1>'
-	#	output += 'Author: ' + str(book.author)
-	#	if book.subject is not None:
-	#		output += 'Subject: ' + str(book.subject)
-	#	if book.category is not None:
-	#		output += 'Category: ' + str(book.category)
-	#	if book.summary is not None:
-	#		output += 'Summary; ' + str(book.summary)
-	#	output += '</br>'
-	#return output
-
 
 # Route for adding a new book
 @app.route('/books/new', methods = ['GET', 'POST'])
@@ -67,9 +45,22 @@ def newBook():
 @app.route('/books/<int:book_id>/edit', methods = ['GET', 'POST'])
 def editBook(book_id):
 	editBook = session.query(Book).filter_by(id = book_id).one()
-	output = ''
-	output += 'Page for editing information of ' + str(editBook.title)
-	return output
+	if request.method == 'POST':
+		if request.form['title']:
+			editBook.title = request.form['title']
+		if request.form['author']:
+			editBook.author = request.form['author']
+		if request.form['subject']:
+			editBook.subject = request.form['subject']
+		if request.form['category']:
+			editBook.category = request.form['category']
+		if request.form['summary']:
+			editBook.summary = request.form['summary']
+		session.add(editBook)
+		session.commit()
+		return redirect(url_for('bookInformation', book_id = book_id))
+	else:
+		return render_template('editBook.html', book_id = book_id, editBook = editBook)
 
 # Route for deleting book information
 @app.route('/books/<int:book_id>/delete', methods = ['GET', 'POST'])

26181f779e426fdbf71397e586a016ac9a8887b9
Bill Kelly <billfk1989@gmail.com>
2018-08-02 16:31:58 -0500

    Add login, gconnect, gdisconnect functions to be able to signin and signout using gmail account

--- BookReviewMain.py
+++ BookReviewMain.py
@@ -108,7 +108,7 @@ def gconnect():
 		response.headers['Content-Type'] = 'application/json'
 		return response
 	# Check that the access token is valid
-	access_token - credentials.access_token
+	access_token = credentials.access_token
 	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' %access_token)
 	h = httplib2.Http()
 	result = json.loads(h.request(url, 'GET')[1])
@@ -129,25 +129,33 @@ def gconnect():
 		response.headers['Content-Type'] = 'application/json'
 		return response
 	# Check to see if user is already logged in
-	stored_credentials = login_session.get('credentials')
+	stored_access_token = login_session.get('access_token')
 	stored_gplus_id = login_session.get('gplus_id')
-	if stored_credentials is not None and gplus_id == stored_gplus_id:
+	if stored_access_token is not None and gplus_id == stored_gplus_id:
 		response = make_response(json.dumps('Current user is already connected.'), 200)
 		response.headers['Content-Type'] = 'application/json'
+		return response
 
 	# Store the access token in the seesion for later use.
-	login_session['credentials'] = credentials
+	login_session['provider'] = 'google'
+	login_session['access_token'] = credentials.access_token
 	login_session['gplus_id'] = gplus_id
 
 	# Get user info
-	userinfo_url = "https://www.googleapis.com/oauth2/v1/uerinfo"
-	params - {'access_token': credentials.access_token, 'alt':'json'}
-	answer = request.get(userinfo_url, params=params)
+	userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+	params = {'access_token': credentials.access_token, 'alt':'json'}
+	answer = requests.get(userinfo_url, params=params)
 	data = json.loads(answer.text)
 
 	login_session['username'] = data["name"]
 	login_session['picture'] = data["picture"]
-	login_session['email'] = data["emial"]
+	login_session['email'] = data["email"]
+
+	# See if user exists in database, if it doesn't make a new one
+	user_id = getUserID(login_session['email'])
+	if not user_id:
+		user_id = createUser(login_session)
+	login_session['user_id'] = user_id
 
 	output = ''
 	output += '<h1>Welcome, '
@@ -156,7 +164,9 @@ def gconnect():
 	output += '<img src="'
 	output += login_session['picture']
 	output += ' " style = width: 300px; height: 300px; border-redius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;">'
+	print "Logged in."
 	return output
+
 @app.route('/gdisconnect')
 def gdisconnect():
 	access_token = login_session.get('access_token')
@@ -168,9 +178,9 @@ def gdisconnect():
 	print 'In gdisconnect access token is %s', access_token
 	print 'User name is: '
 	print login_session['username']
-	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
 	h = httplib2.Http()
-	result - h.request(url, 'GET')[0]
+	result = h.request(url, 'GET')[0]
 	print 'result is '
 	print result
 	if result['status'] == '200':
@@ -179,7 +189,7 @@ def gdisconnect():
 		del login_session['username']
 		del login_session['email']
 		del login_session['picture']
-		response = make_response(json.dumps('Successfully disconnecte.'), 200)
+		response = make_response(json.dumps('Successfully disconnected.'), 200)
 		response.headers['Content-Type'] = 'application/json'
 		return response
 	else:
@@ -203,8 +213,8 @@ def getUserInfo(user_id):
 #Create a new user in database
 def createUser(login_session):
 	newUser = User(name = login_session['username'],
-	email = login_session['email'],
-	picture = login_session['picture'])
+		email = login_session['email'],
+		picture = login_session['picture'])
 	session.add(newUser)
 	session.commit()
 	user = session.query(User).filter_by(email = login_session['email']).one()

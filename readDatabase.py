35bb146199515345f1e695bda284ea616121a8ea
Bill Kelly <billfk1989@gmail.com>
2018-08-31 16:04:25 -0500

    Add forums to read

--- readDatabase.py
+++ readDatabase.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from BookReviewDatabase import Base, User, Book
+from BookReviewDatabase import Base, User, Book, Forum
 engine = create_engine('sqlite:///bookreview.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
@@ -11,10 +11,16 @@ for book in books:
 	print book.title
 	print book.user
 	print book.id
-	print 
+print
 users = session.query(User).all()
 print ""
 print "Users:"
 for user in users:
 	print user.email
 	print user.id
+print
+forums = session.query(Forum).all()
+print "Forums:"
+for forum in forums:
+	print forum.id
+	print forum.title

98262800376ac06e6630bd17b3e4fcdc0cc09898
Bill Kelly <billfk1989@gmail.com>
2018-09-07 12:49:43 -0500

    Add BookForumConnect read

--- readDatabase.py
+++ readDatabase.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from BookReviewDatabase import Base, User, Book, Forum
+from BookReviewDatabase import Base, User, Book, Forum, BookForumConnect
 engine = create_engine('sqlite:///bookreview.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
@@ -24,3 +24,8 @@ print "Forums:"
 for forum in forums:
 	print forum.id
 	print forum.title
+connects = session.query(BookForumConnect).all()
+print "Connects:"
+for connect in connects:
+	print connect.book_id
+	print connect.forum_id
